1067592a0375ed4c79880d309f7e7fafd5cd9886
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-06-03 16:40:48 +0330

    refactor component App done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import HomePage from "./components/HomePage";
+import image from "@assets/image/pattern.png";
+import { Grid } from "@mui/material";
 function App() {
+  const style = {
+    backgroundImage: `url(${image})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  };
   return (
-    <div>
-      App
-    </div>
-  )
+    <Grid direction={'rtl'} display={'flex'}
+    justifyContent={'center'}>
+      <Grid bgcolor={'#f2f2f2'}height={'auto'} width={'100%'} maxWidth={500} style={style}>
+        <HomePage />
+      </Grid>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
