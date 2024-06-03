c281cbb134b99d326eaf25ef78f5ffabdacd9e2e
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-06-03 18:13:31 +0330

    update component app done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import HomePage from "./components/HomePage";
 import image from "@assets/image/pattern.png";
 import { Grid } from "@mui/material";
+import FinalMessage from "./components/FinalMessage";
+
 function App() {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
   const style = {
     backgroundImage: `url(${image})`,
     backgroundPosition: "center",
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
   };
+
   return (
-    <Grid direction={'rtl'} display={'flex'}
-    justifyContent={'center'}>
-      <Grid bgcolor={'#f2f2f2'}height={'auto'} width={'100%'} maxWidth={500} style={style}>
-        <HomePage />
+    <Grid direction={"rtl"} display={"flex"} justifyContent={"center"}>
+      <Grid
+        bgcolor={"#f2f2f2"}
+        height={"auto"}
+        width={"100%"}
+        maxWidth={500}
+        style={style}
+      >
+        {isSubmitted ? (
+          <FinalMessage />
+        ) : (
+          <HomePage setIsSubmitted={setIsSubmitted} />
+        )}
       </Grid>
     </Grid>
   );
 }
 
 export default App;
+
