d2223699942efee6ddd5438fab93c5f6f5bf7d97
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-06-04 14:41:02 +0330

    uppdate component HomePage done

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,23 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
-  Container,
   TextField,
   Button,
-  Typography,
   Box,
   InputAdornment,
-  Dialog,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
   IconButton,
 } from "@mui/material";
+
 import { useFormik } from "formik";
 import * as yup from "yup";
 import axios from "axios";
-import { AccountCircle, Phone, Instagram, Lock ,Refresh} from "@mui/icons-material";
-import userImage from "@assets/image/imageuser.jpg"; // Import the user image
-import SendIcon from '@mui/icons-material/Send';
+import { Lock, Refresh } from "@mui/icons-material";
+
+import SendIcon from "@mui/icons-material/Send";
+import initialFields from "../data/initialFieldsInputs";
+import DialogMessage from "./DialogMessage";
+import BannerPage from "./BannerPage";
+
 // Function to convert Persian numbers to English numbers
 const convertPersianToEnglish = (str) => {
   const persianDigits = "۰۱۲۳۴۵۶۷۸۹";
@@ -28,43 +27,67 @@ const convertPersianToEnglish = (str) => {
   );
 };
 
+// Function to generate random captcha code
+const fetchCaptcha = async () => {
+  const response = await axios.get("http://api.mantrayou.com:5050/gen/captcha");
+  return response.data;
+};
+
 const validationSchema = yup.object({
-  name: yup
+  fullname: yup
     .string("نام و نام خانوادگی خود را وارد کنید")
     .required("وارد کردن نام و نام خانوادگی الزامی است"),
-  phone: yup
+  phone_number: yup
     .string("شماره همراه خود را وارد کنید")
     .transform((value) => convertPersianToEnglish(value)) // Convert Persian digits to English
     .matches(/^[0-9]{11}$/, "شماره همراه باید ۱۱ رقم باشد")
     .required("وارد کردن شماره همراه الزامی است"),
-  instagram: yup
+  instagram_id: yup
     .string("آی دی اینستاگرام خود را وارد کنید")
     .required("وارد کردن آی دی اینستاگرام الزامی است"),
-  captcha: yup
+  captcha_code: yup
     .string("کد روبه رو را وارد کنید")
     .required("وارد کردن کد روبه رو الزامی است"),
 });
 
-function HomePage({ setIsSubmitted }) {
+function HomePage({ setIsSubmitted, setlLotteryId }) {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [submitError, setSubmitError] = useState(false);
+  const [captcha, setCaptcha] = useState(null);
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+  const fetchCaptchaData = async () => {
+    const captchaData = await fetchCaptcha();
+    setCaptcha(captchaData);
+  };
+
+  useEffect(() => {
+    // Generate a new captcha when the component is mounted
+    fetchCaptchaData();
+  }, []);
 
   const formik = useFormik({
     initialValues: {
-      name: "",
-      phone: "",
-      instagram: "",
-      captcha: "",
+      fullname: "",
+      phone_number: "",
+      instagram_id: "",
+      captcha_code: "",
     },
     validationSchema: validationSchema,
     onSubmit: (values, { resetForm }) => {
       setIsSubmitting(true);
+      console.log({ ...values, captcha_id: captcha?.captcha_id });
       axios
-        .post("YOUR_BACKEND_URL", values)
+        .post(`${BASE_URL}/form/submit`, {
+          ...values,
+          captcha_id: captcha?.captcha_id,
+        }) // Include captcha_id in the submission if needed
         .then((response) => {
           setIsSubmitting(false);
           setIsSubmitted(true);
+          setlLotteryId(response);
           resetForm();
+          fetchCaptchaData(); // Fetch new captcha after successful submission
         })
         .catch((error) => {
           setIsSubmitting(false);
@@ -89,8 +112,8 @@ function HomePage({ setIsSubmitted }) {
         borderColor: "#aaa",
       },
       "& input": {
-        padding: "10px", // Adjust padding to decrease height
-        fontFamily: "vazir"
+        padding: "15px", // Adjust padding to decrease height
+        fontFamily: "vazir",
       },
     },
   };
@@ -98,213 +121,94 @@ function HomePage({ setIsSubmitted }) {
   const helperTextStyles = {
     textAlign: "right",
     marginRight: "10px",
-    fontFamily:'vazir'
+    fontFamily: "vazir",
   };
-  const handleCaptchaRefresh = () => {
-    // Add your captcha refresh logic here
-    console.log("Captcha refreshed");
-  };
-  const submittingDialogStyles = {
-    "& .MuiDialog-paper": {
-      backgroundColor: "#e0f7fa",
-      color: "#006064",
-      padding: "20px",
-      textAlign:'center'
-    },
-    "& .MuiDialogTitle-root": {
-      fontWeight: "bold",
-      fontFamily:'vazir'
-    },
-  };
-
-  const errorDialogStyles = {
-    "& .MuiDialog-paper": {
-      backgroundColor: "#ffebee",
-      color: "#c62828",
-      padding: "20px",
-      textAlign:'center',
-      
-    },
-    "& .MuiDialogTitle-root": {
-      fontWeight: "bold",
-      fontFamily:'vazir'
-    },
-  };
+console.log(formik.touched,'formik.touched')
   return (
     <>
-      <Box textAlign="center">
-        <img
-          src={userImage}
-          alt="Event"
-          style={{
-            width: "92%",
-            height: "450px",
-            borderRadius: "0 0 200% 200%",
-          }}
-        />
-        <Typography
-          variant="h4"
-          py={5}
-          fontFamily={"vazir"}
-          fontWeight={"bolder"}
-          color={"#A32098"}
-        >
-          رویداد بزرگ نیکینا و نگین
-        </Typography>
-        <Typography
-          fontFamily={"vazir"}
-          fontSize={"16px"}
-          fontWeight={"bold"}
-          color={"#444"}
-          lineHeight={"28px"}
-          padding={"5px 20px"}
-          textAlign={"justify"}
-        >
-          عزیزای دلم دقت کنید برای شرکت در قرعه کشی ابتدا همه کسانی که من فالو
-          کردم رو فالو کنید بعد مشخصات خودتون رو در فرم پایین صفحه تکمیل کنید
-          وقتی ثبت شد یه کد 6 رقمی نمایش داده میشه، اونو تا پایان مسابقه پیش
-          خودتون نگه دارید میتونید از صفحه اسکرین شات بگیرید لازم به ذکر هست هر
-          نفر با هر شماره فقط یک بار می تونه شرکت کنه
-        </Typography>
-      </Box>
+      <BannerPage />
       <form onSubmit={formik.handleSubmit} style={{ padding: "0px 20px" }}>
-        <TextField
-          placeholder="نام و نام خانوادگی"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          name="name"
-          value={formik.values.name}
-          onChange={formik.handleChange}
-          error={formik.touched.name && Boolean(formik.errors.name)}
-          helperText={formik.touched.name && formik.errors.name}
-          FormHelperTextProps={{ style: helperTextStyles }}
-          sx={textFieldStyles}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <AccountCircle sx={{color:'#ddd' }} />
-              </InputAdornment>
-            ),
-          }}
-        />
-        <TextField
-          placeholder="شماره همراه"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          name="phone"
-          value={formik.values.phone}
-          onChange={formik.handleChange}
-          error={formik.touched.phone && Boolean(formik.errors.phone)}
-          helperText={formik.touched.phone && formik.errors.phone}
-          FormHelperTextProps={{ style: helperTextStyles }}
-          sx={textFieldStyles}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <Phone  sx={{color:'#ddd' }}/>
-              </InputAdornment>
-            ),
-            // sx: { fontFamily: "vazir" },
-          }}
-        />
-        <TextField
-          placeholder="آی دی اینستاگرام"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          name="instagram"
-          value={formik.values.instagram}
-          onChange={formik.handleChange}
-          error={formik.touched.instagram && Boolean(formik.errors.instagram)}
-          helperText={formik.touched.instagram && formik.errors.instagram}
-          FormHelperTextProps={{ style: helperTextStyles }}
-          sx={textFieldStyles}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <Instagram  sx={{color:'#ddd' }}/>
-              </InputAdornment>
-            ),
-            // sx: { fontFamily: "vazir" },
-          }}
-        />
+        {initialFields.map((item) => (
+          <TextField
+            key={item?.id}
+            placeholder={item?.placeholder}
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            name={item?.name}
+            value={formik.values.item?.name}
+            onChange={formik.handleChange}
+            error={
+              formik.touched[item?.name] && Boolean(formik.errors[item?.name])
+            }
+            helperText={formik.touched[item?.name] && formik.errors[item?.name]}
+            FormHelperTextProps={{ style: helperTextStyles }}
+            sx={textFieldStyles}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">{item?.icon}</InputAdornment>
+              ),
+            }}
+          />
+        ))}
+
         <Box display="flex" alignItems="center">
           <TextField
             placeholder="کد روبه رو"
             variant="outlined"
             fullWidth
             margin="normal"
-            name="captcha"
-            value={formik.values.captcha}
+            name="captcha_code"
+            value={formik.values.captcha_code}
             onChange={formik.handleChange}
-            error={formik.touched.captcha && Boolean(formik.errors.captcha)}
-            helperText={formik.touched.captcha && formik.errors.captcha}
+            error={
+              formik.touched.captcha_code && Boolean(formik.errors.captcha_code)
+            }
+            helperText={
+              formik.touched.captcha_code && formik.errors.captcha_code
+            }
             FormHelperTextProps={{ style: helperTextStyles }}
             sx={textFieldStyles}
             InputProps={{
               startAdornment: (
                 <InputAdornment position="start">
-                  <Lock  sx={{color:'#ddd' }}/>
+                  <Lock sx={{ color: "#ddd" }} />
                 </InputAdornment>
               ),
               endAdornment: (
                 <InputAdornment position="end">
-                  <IconButton onClick={handleCaptchaRefresh}>
-                   
+                  <IconButton onClick={fetchCaptchaData}>
                     <Refresh sx={{ color: "#ddd" }} />
                   </IconButton>
-                  675876
+                  {captcha && (
+                    <img src={`${BASE_URL}/${captcha?.link}`} alt="captcha" />
+                  )}
                 </InputAdornment>
               ),
-            //   sx: { fontFamily: "vazir" },
             }}
-            
           />
-          {/* <Box mx={2} p={2} border={1} borderColor="grey.400">
-            <Typography variant="h6">۶۷۲</Typography>
-          </Box> */}
         </Box>
         <Box mb={4}>
-          <Button type="submit" variant="contained" sx={{backgroundColor:"#a31299",gap:'10px',fontFamily:' vazir' }} fullWidth>
-            ارسال<SendIcon sx={{fontSize:'16px'}}/>
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{
+              backgroundColor: "#a31299",
+              gap: "10px",
+              fontFamily: " vazir",
+            }}
+            fullWidth
+          >
+            ارسال
+            <SendIcon sx={{ fontSize: "16px" }} />
           </Button>
         </Box>
       </form>
-
-      <Dialog
-        open={isSubmitting}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-        sx={submittingDialogStyles}
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"در حال بررسی اطلاعات"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            لطفاً منتظر بمانید، اطلاعات شما در حال بررسی است.
-          </DialogContentText>
-        </DialogContent>
-      </Dialog>
-
-      <Dialog
-        open={submitError}
-        onClose={() => setSubmitError(false)}
-        aria-labelledby="alert-dialog-error-title"
-        aria-describedby="alert-dialog-error-description"
-        sx={errorDialogStyles}
-      >
-        <DialogTitle id="alert-dialog-error-title">
-          {"خطا در ارسال اطلاعات"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-error-description">
-            مشکلی در ارسال اطلاعات به وجود آمد. لطفاً دوباره تلاش کنید.
-          </DialogContentText>
-        </DialogContent>
-      </Dialog>
+      <DialogMessage
+        submitError={submitError}
+        isSubmitting={isSubmitting}
+        setSubmitError={setSubmitError}
+      />
     </>
   );
 }
